cbd2fbeb1072c883dcda490f7c2dce7a6f458bea
June <sirawitjunewicha@gmail.com>
2025-07-15 12:29:16 +0700

    Migrate ESlint to v9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,68 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 });
 
-export default [
+const eslintConfig = [
+  {
+    ignores: ["components/ui/**/*"],
+  },
   ...compat.extends(
     "next/core-web-vitals",
     "next/typescript",
     "standard",
-    //"plugin:tailwindcss/recommended",
     "prettier"
+    // "plugin:tailwindcss/recommended"
   ),
   {
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+          ],
+
+          "newlines-between": "always",
+
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ["builtin"],
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "comma-dangle": "off",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+
     rules: {
       "no-undef": "off",
     },
   },
 ];
+
+export default eslintConfig;
